23ae7e0bab605228c0995fa75fe1d6039092247e
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-11-06 18:21:07 +0300

    add tests to Item class

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,23 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def test_item():
+    return Item("iPhone", 80000, 3)
+
+
+def test_item_init(test_item):
+    name, price, num = test_item.name, test_item.price, test_item.quantity
+    assert f"{name} {price} {num}" == "iPhone 80000 3"
+
+
+def test_calculate_total_price(test_item):
+    assert test_item.calculate_total_price() == 240000
+
+
+def test_apply_discount(test_item):
+    Item.pay_rate = 0.8
+    test_item.apply_discount()
+    assert test_item.price == 64000
